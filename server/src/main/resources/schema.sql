ebaa59075ce70be9de496798ab9cf1b10baf4bac
Munish Chandel <cancerian0684@gmail.com>
2014-11-25 23:37:20 +0530

    Remember me option using cookies

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE PUBLIC.USERS
+CREATE TABLE if not exists PUBLIC.USERS
 (
   USERNAME VARCHAR(50) PRIMARY KEY NOT NULL,
   PASSWORD VARCHAR(50) NOT NULL,
   ENABLED BOOLEAN DEFAULT TRUE
 );
 
-CREATE TABLE PUBLIC.AUTHORITIES
+CREATE TABLE if not exists PUBLIC.AUTHORITIES
 (
   username varchar_ignorecase (50) NOT NULL,
   authority varchar_ignorecase (50) NOT NULL,
@@ -13,3 +13,5 @@ CREATE TABLE PUBLIC.AUTHORITIES
 );
 
 CREATE UNIQUE INDEX ix_auth_username ON PUBLIC.AUTHORITIES (username, authority);
+
+create table if not exists persistent_logins (username varchar(64) not null, series varchar(64) primary key, token varchar(64) not null, last_used timestamp not null)
