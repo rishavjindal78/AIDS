b60ecb733968d7a1eccc15e7af7bbc5a9bf3ff11
Munish Chandel <cancerian0684@gmail.com>
2014-12-29 16:10:05 +0530

    Fixed Unzip File/Folder issue

--- shared/src/main/java/org/shunya/shared/taskSteps/UnZipStep.java
+++ shared/src/main/java/org/shunya/shared/taskSteps/UnZipStep.java
@@ -5,12 +5,12 @@ import org.shunya.shared.annotation.InputParam;
 import org.shunya.shared.annotation.OutputParam;
 import org.shunya.shared.annotation.PunterTask;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 @PunterTask(author = "munishc", name = "UnZipStep", description = "Echo's the input data to SOP", documentation = "docs/TextSamplerDemoHelp.html")
@@ -24,37 +24,64 @@ public class UnZipStep extends AbstractStep {
     @Override
     public boolean run() {
         LOGGER.get().log(Level.INFO, "unzipping File " + inputFile + " to location - " + outputFolder);
-        return unZipIt(inputFile, outputFolder);
+        return extractFolder(inputFile, outputFolder);
     }
 
-    public boolean unZipIt(String zipFile, String outputFolder) {
-        byte[] buffer = new byte[1024];
+    public boolean extractFolder(String zipFile, String outputFolder) {
         try {
             File folder = new File(outputFolder);
             if (!folder.exists()) {
                 folder.mkdir();
             }
-            ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
-            ZipEntry ze = zis.getNextEntry();
-            while (ze != null) {
-                String fileName = ze.getName();
-                File newFile = new File(outputFolder + File.separator + fileName);
-                System.out.println("file unzip : " + newFile.getAbsoluteFile());
-                new File(newFile.getParent()).mkdirs();
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
+            System.out.println(zipFile);
+            int BUFFER = 2048;
+            File file = new File(zipFile);
+            ZipFile zip = new ZipFile(file);
+            Enumeration zipFileEntries = zip.entries();
+            // Process each entry
+            while (zipFileEntries.hasMoreElements()) {
+                // grab a zip file entry
+                ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
+                String currentEntry = entry.getName();
+                File destFile = new File(folder, currentEntry);
+                //destFile = new File(newPath, destFile.getName());
+                File destinationParent = destFile.getParentFile();
+
+                // create the parent directory structure if needed
+                destinationParent.mkdirs();
+
+                if (!entry.isDirectory()) {
+                    BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
+                    int currentByte;
+                    // establish buffer for writing file
+                    byte data[] = new byte[BUFFER];
+
+                    // write the current file to disk
+                    FileOutputStream fos = new FileOutputStream(destFile);
+                    BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER);
+
+                    // read and write until last byte is encountered
+                    while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
+                        dest.write(data, 0, currentByte);
+                    }
+                    dest.flush();
+                    dest.close();
+                    is.close();
                 }
-                fos.close();
-                ze = zis.getNextEntry();
+
+                if (currentEntry.endsWith(".zip")) {
+                    return extractFolder(destFile.getAbsolutePath(), outputFolder);
+                }
             }
-            zis.closeEntry();
-            zis.close();
             return true;
         } catch (IOException ex) {
             LOGGER.get().log(Level.SEVERE, "Error in zip file extraction - ", ex);
         }
         return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        UnZipStep unZipStep = new UnZipStep();
+        unZipStep.extractFolder("C:\\aids_home\\test.zip", "C:\\aids_home\\testMe");
     }
 }
