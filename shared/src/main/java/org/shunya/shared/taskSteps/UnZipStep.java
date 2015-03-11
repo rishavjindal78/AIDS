f9b87f250b934cc3864ae59d3ad977f012359317
Munish Chandel <cancerian0684@gmail.com>
2015-03-11 14:11:41 +0530

    bug fix : closing the zip file at the end

--- shared/src/main/java/org/shunya/shared/taskSteps/UnZipStep.java
+++ shared/src/main/java/org/shunya/shared/taskSteps/UnZipStep.java
@@ -47,20 +47,16 @@ public class UnZipStep extends AbstractStep {
                 File destFile = new File(folder, currentEntry);
                 //destFile = new File(newPath, destFile.getName());
                 File destinationParent = destFile.getParentFile();
-
                 // create the parent directory structure if needed
                 destinationParent.mkdirs();
-
                 if (!entry.isDirectory()) {
                     BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
                     int currentByte;
                     // establish buffer for writing file
                     byte data[] = new byte[BUFFER];
-
                     // write the current file to disk
                     FileOutputStream fos = new FileOutputStream(destFile);
                     BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER);
-
                     // read and write until last byte is encountered
                     while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
                         dest.write(data, 0, currentByte);
@@ -69,11 +65,11 @@ public class UnZipStep extends AbstractStep {
                     dest.close();
                     is.close();
                 }
-
                 if (recursive && currentEntry.endsWith(".zip")) {
                     extractFolder(destFile.getAbsolutePath(), outputFolder, recursive);
                 }
             }
+            zip.close();
             return true;
         } catch (IOException ex) {
             LOGGER.get().log(Level.SEVERE, "Error in zip file extraction - ", ex);
